a2b02657596b14233acfb703806a071c811ec678
miguelabragonz <55327101+miguelabragonz@users.noreply.github.com>
2019-10-05 10:41:18 -0500

    Update mathmaticalOperationsDebug.py

--- mathmaticalOperationsDebug.py
+++ mathmaticalOperationsDebug.py
@@ -4,17 +4,17 @@ Created on Oct 4, 2019
 @author: ITAUser
 '''
 
-40 * 0 = 0
+40 * 0 = 0                  zero times anything is zero 
 
-b. 
+"Hello 1 2 and" + 3         cant't add strings with int.
 
-6 * 5 + 1 = 31
+6 * 5 + 1 = 31              six times five is thirty and plus one is thirty one
 
-d.
+print(10 / 2)               remove quotes
 
-e.
+print("lol" * 3)            it's fine
 
-f. 
+print( "1" + "2" + "3")     lowercase P in the print
 
-10 % 2 = 0
+10 % 2 == 0                 2 goes into 10 evenly so 0 is trhe remainder 
 
