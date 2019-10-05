15aa4bc027950d3790e322a27441ba2a6cd2de28
miguelabragonz <55327101+miguelabragonz@users.noreply.github.com>
2019-10-05 10:27:25 -0500

    Initial commit

---
+++ mathmaticalOperationsDrill.py
@@ -0,0 +1,60 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+'''Addition'''
+a = 5 + 6
+
+
+a = 3
+
+b = 4
+
+c = a + b
+
+'''Subtraction'''
+a = 10 - 5
+
+
+a = 9
+b = 19
+
+c = b - a
+
+'''Multiplication'''
+a = 2 * 3 
+
+a = 6 
+b = 9
+
+c = a * b
+print(c)
+
+'''Division'''
+x = 10 / 2
+
+x = 15
+y = 3
+
+z = x / y
+print(z)
+
+'''Division(floor)'''
+x = 10 // 3
+
+x = 25
+y = 7
+
+w = x // y
+print(w)
+
+'''Modulus'''
+q = 32 % 3
+
+r = 60
+s = 8 
+
+q = r % s
+print(q)
+
---
+++ practiceMathmaticalOperations.py
@@ -0,0 +1,40 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+a = 2 + 3
+
+a = 2
+b = 3
+
+c = a + b
+
+b = "Miguel"
+c = "is awesome"
+
+d = b + " " + c
+print(d)
+
+d = b + "is cool"
+print(d)
+
+x = 2
+y = 3
+
+z = x * y
+print(z)
+
+z = x / y
+print(z)
+
+z = x // y
+print(z)
+
+d = 20
+e = 6
+f = d // e
+print("f equals" + str(f))
+
+f = d % e 
+print("f remainder" +str(f))
